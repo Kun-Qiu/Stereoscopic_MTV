f073771cfe99008925b49a5dad8a4b0fbe80fa3d
Kun Qiu <kun.qiu@vanderbilt.edu>
2024-02-26 10:54:35 -0600

    template matching

--- Utility/Template.py
+++ Utility/Template.py
@@ -1,60 +1,60 @@
-import numpy as np
 import cv2
 import argparse
 
 
-class TemplateDrawer:
-    def __init__(self, image_path, template_path):
-        self.image_path = image_path
-        self.template_path = template_path
-        self.drawing = False
-        self.ix, self.iy = -1, -1
-        self.img = cv2.imread(image_path)
-        self.clone = self.img.copy()
+class ImageLineDrawer:
+    def __init__(self, image_path, size_multiplier):
+        self.image = cv2.imread(image_path)
+        self.image = cv2.resize(self.image, (0, 0), fx=size_multiplier, fy=size_multiplier)
+        self.points = []
+        self.size_multiplier = size_multiplier
 
-    def draw_line(self, event, x, y, flags, param):
-        if event == cv2.EVENT_LBUTTONDOWN:
-            self.drawing = True
-            self.ix, self.iy = x, y
+    def draw_line(self, pt1, pt2, color=(0, 255, 0), thickness=1):
+        return cv2.line(self.image, pt1, pt2, color, thickness)
 
-        elif event == cv2.EVENT_MOUSEMOVE:
-            if self.drawing:
-                cv2.line(self.img, (self.ix, self.iy), (x, y), (0, 0, 255), 2)
-                self.ix, self.iy = x, y
+    def get_points(self):
+        print("Please pick two points on the image by clicking on them. Press 'q' to quit.")
 
-        elif event == cv2.EVENT_LBUTTONUP:
-            self.drawing = False
-            cv2.line(self.img, (self.ix, self.iy), (x, y), (0, 0, 255), 2)
+        def mouse_callback(event, x, y, flags, param):
+            if event == cv2.EVENT_LBUTTONDOWN:
+                self.points.append((x, y))
+                if len(self.points) == 2:
+                    cv2.destroyAllWindows()
 
-    def save_template(self):
-        cv2.imwrite(self.template_path, self.img)
-        print("Template saved successfully!")
+        cv2.imshow('Image', self.image)
+        cv2.setMouseCallback('Image', mouse_callback)
+        key = cv2.waitKey(0)
+        if key & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
 
-    def draw_template(self):
-        cv2.namedWindow("Select Template")
-        cv2.setMouseCallback("Select Template", self.draw_line)
+    def draw_lines(self):
+        self.get_points()
+        self.draw_line(self.points[0], self.points[1])
+        self.points.clear()  # Clear the points list after drawing the first line
 
-        while True:
-            cv2.imshow("Select Template", self.img)
-            key = cv2.waitKey(1) & 0xFF
-
-            if key == ord("s"):
-                self.save_template()
-                break
-            elif key == ord("r"):
-                self.img = self.clone.copy()
-
+        self.get_points()
+        final_image_with_second_line = self.draw_line(self.points[0], self.points[1])
+        cv2.imshow('Final Image', final_image_with_second_line)
+        cv2.waitKey(0)
         cv2.destroyAllWindows()
+
+        return final_image_with_second_line
+
+    def save_lines_image(self, image_path):
+        lines_image = self.draw_lines()
+        self.image = cv2.resize(self.image, (0, 0), fx=1/self.size_multiplier,
+                                fy=1/self.size_multiplier)
+        cv2.imwrite(image_path, lines_image)
+        print(f"Image with only lines drawn saved as {image_path}")
 
 
 if __name__ == "__main__":
-    # Construct the argument parser and parse the arguments
-    ap = argparse.ArgumentParser()
-    ap.add_argument("-i", "--image", type=str, required=True,
-                    help="path to input image where we'll draw the template")
-    ap.add_argument("-t", "--template", type=str, required=True,
-                    help="path to save the template image")
-    args = vars(ap.parse_args())
+    parser = argparse.ArgumentParser(description='Draw lines on an image')
+    parser.add_argument('image_path', type=str, help='Path to the input image file')
+    parser.add_argument('save_path', type=str, help='Path to save image')
+    parser.add_argument('--multi', type=float, default=2,
+                        help='Multiplier to resize the image (default: 2)')
+    args = parser.parse_args()
 
-    drawer = TemplateDrawer(args["image"], args["template"])
-    drawer.draw_template()
+    image_drawer = ImageLineDrawer(args.image_path, args.multi)
+    image_drawer.save_lines_image(args.save_path)
