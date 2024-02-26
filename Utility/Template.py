3c518473f4194349407e28d0a30494c86f5f1561
Kun Qiu <kun.qiu@vanderbilt.edu>
2024-02-26 13:44:46 -0600

    Template Finding Done

--- Utility/Template.py
+++ Utility/Template.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 import argparse
 
 
@@ -8,6 +9,7 @@ class ImageLineDrawer:
         self.image = cv2.resize(self.image, (0, 0), fx=size_multiplier, fy=size_multiplier)
         self.points = []
         self.size_multiplier = size_multiplier
+        self.size = np.shape(self.image)
 
     def draw_line(self, pt1, pt2, color=(0, 255, 0), thickness=1):
         return cv2.line(self.image, pt1, pt2, color, thickness)
@@ -18,33 +20,49 @@ class ImageLineDrawer:
         def mouse_callback(event, x, y, flags, param):
             if event == cv2.EVENT_LBUTTONDOWN:
                 self.points.append((x, y))
-                if len(self.points) == 2:
+                if len(self.points) == 4:
                     cv2.destroyAllWindows()
+                print(x, y)
 
         cv2.imshow('Image', self.image)
         cv2.setMouseCallback('Image', mouse_callback)
-        key = cv2.waitKey(0)
-        if key & 0xFF == ord('q'):
-            cv2.destroyAllWindows()
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
     def draw_lines(self):
         self.get_points()
         self.draw_line(self.points[0], self.points[1])
-        self.points.clear()  # Clear the points list after drawing the first line
+        self.draw_line(self.points[2], self.points[3])
 
-        self.get_points()
-        final_image_with_second_line = self.draw_line(self.points[0], self.points[1])
-        cv2.imshow('Final Image', final_image_with_second_line)
+        cv2.imshow('Final Image', self.image)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
-
-        return final_image_with_second_line
+        return self.image
 
     def save_lines_image(self, image_path):
-        lines_image = self.draw_lines()
-        self.image = cv2.resize(self.image, (0, 0), fx=1/self.size_multiplier,
-                                fy=1/self.size_multiplier)
-        cv2.imwrite(image_path, lines_image)
+        self.draw_lines()
+
+        # Create a blank image with the calculated dimensions
+        lines_image = np.zeros(np.shape(self.image), dtype=np.uint8)
+
+        # Draw the lines on the blank image
+        cv2.line(lines_image, (self.points[0][0], self.points[0][1]),
+                 (self.points[1][0], self.points[1][1]), (0, 255, 0), 1)
+        cv2.line(lines_image, (self.points[2][0], self.points[2][1]),
+                 (self.points[3][0], self.points[3][1]), (0, 255, 0), 1)
+
+        cv2.resize(lines_image, (self.image.shape[1], self.image.shape[0]))
+
+        min_x = min(self.points[0][0], self.points[1][0], self.points[2][0], self.points[3][0])
+        max_x = max(self.points[0][0], self.points[1][0], self.points[2][0], self.points[3][0])
+        min_y = min(self.points[0][1], self.points[1][1], self.points[2][1], self.points[3][1])
+        max_y = max(self.points[0][1], self.points[1][1], self.points[2][1], self.points[3][1])
+
+        # Crop the image to the bounding box
+        cropped_image = lines_image[min_y:max_y, min_x:max_x]
+
+        # Save the lines as an image
+        cv2.imwrite(image_path, cropped_image)
         print(f"Image with only lines drawn saved as {image_path}")
 
 
