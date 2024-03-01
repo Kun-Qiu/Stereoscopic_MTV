c481c9804e4f902afa012ae1e903be8fa66669bd
Kun Qiu <kun.qiu@vanderbilt.edu>
2024-03-01 16:41:10 -0600

    added intersection point

--- TemplateMatching.py
+++ TemplateMatching.py
@@ -6,15 +6,16 @@ from Utility.DeNoise import denoised_image
 # Load the images
 img = cv2.imread('Data/Source/source.png')
 img_gray = denoised_image('Data/Source/source.png')
+
+img2 = cv2.imread('Data/Target/target.png')
+img_target = denoised_image('Data/Target/target.png')
 temp = cv2.imread('Data/Template/temp.jpg')
 temp_gray = cv2.cvtColor(temp, cv2.COLOR_RGB2GRAY)
 
-# Apply Tsutomu's thresholding
-_, img_thresh = cv2.threshold(img_gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+intersection = np.loadtxt('Data/Template/intersection.txt')
 
-# cv2.imshow("img", img_thresh)
-# cv2.imshow("temp", cv2.resize(temp_gray, (520, 520)))
-# cv2.waitKey()
+# Apply Tsutomu's thresholding
+_, img_thresh = cv2.threshold(img_target, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
 # save the image dimensions
 W, H = temp.shape[:2]
@@ -37,21 +38,24 @@ for (x, y) in zip(x_points, y_points):
     cv2.rectangle(clone, (x, y), (x + W, y + H), (0, 255, 0), 3)
 cv2.imshow("Before NMS", clone)
 
-clone2 = img.copy()
-dx = 0.2
+clone2 = img2.copy()
+dx = 0.4
 rects = []
+
 # loop over the starting (x, y)-coordinates again
 for (x, y) in zip(x_points, y_points):
     # update our list of rectangles
     rects.append((x, y, x + dx * W, y + dx * H))
 # apply non-maxima suppression to the rectangles
+
 pick = non_max_suppression(np.array(rects))
 print("[INFO] {} matched locations *after* NMS".format(len(pick)))
 # loop over the final bounding boxes
 for (startX, startY, endX, endY) in pick:
     # draw the bounding box on the image
-    cv2.rectangle(clone2, (startX, startY), (endX, endY),
-                  (255, 0, 0), 3)
+    # cv2.rectangle(clone2, (startX, startY), (endX, endY),
+    #               (255, 0, 0), 3)
+    cv2.circle(clone2, (startX + int(intersection[0]), startY + int(intersection[1])), 2, (0, 255, 0), -1)
 # show the output image
 cv2.imshow("After NMS", clone2)
 cv2.waitKey(0)
--- Utility/Template.py
+++ Utility/Template.py
@@ -2,6 +2,11 @@ import cv2
 import numpy as np
 import argparse
 
+"""
+ImageLineDrawer is a class use to determine the 
+template image for the template matching
+"""
+
 
 class ImageLineDrawer:
     def __init__(self, image_path, size_multiplier):
@@ -10,10 +15,22 @@ class ImageLineDrawer:
         self.points = []
         self.multiplier = size_multiplier
 
-    def draw_line(self, img, pt1, pt2, color=(0, 255, 0), thickness=5):
+    def draw_line(self, img, pt1, pt2, color=(0, 255, 0), thickness=4):
+        """
+        :param img: image
+        :param pt1: first point on the line
+        :param pt2: second point on the line
+        :param color: color of the line
+        :param thickness: thickness of the line
+        :return: draw the line on the input img
+        """
         return cv2.line(img, pt1, pt2, color, thickness)
 
     def get_points(self):
+        """
+        Function allows the user to select points on the image
+        :return: None
+        """
         print("Please pick two points on the image by clicking on them. Press 'q' to quit.")
 
         def mouse_callback(event, x, y, flags, param):
@@ -29,6 +46,11 @@ class ImageLineDrawer:
         cv2.destroyAllWindows()
 
     def draw_lines(self):
+        """
+        Lines are drawn on the image using the points obtained
+        in mouse click event
+        :return: None
+        """
         self.get_points()
         copied_image = self.resized_image.copy()
         self.draw_line(copied_image,
@@ -44,20 +66,13 @@ class ImageLineDrawer:
         cv2.destroyAllWindows()
 
     def save_lines_image(self, image_path):
+        """
+        Draw the lines on the image, crop the desired section for template,
+        save the intersection point.
+        :param image_path: Path which image will be saved
+        :return: None
+        """
         self.draw_lines()
-
-        # # Create a blank image with the calculated dimensions
-        # lines_image = np.zeros(np.shape(self.resized_image), dtype=np.uint8)
-        #
-        # # Draw the lines on the blank image
-        # cv2.line(lines_image,
-        #          (self.points[0][0], self.points[0][1]),
-        #          (self.points[1][0], self.points[1][1]),
-        #          (0, 255, 0), 1)
-        # cv2.line(lines_image,
-        #          (self.points[2][0], self.points[2][1]),
-        #          (self.points[3][0], self.points[3][1]),
-        #          (0, 255, 0), 1)
 
         gray_img = cv2.cvtColor(self.resized_image, cv2.COLOR_RGB2GRAY)
         _, img_thresh = cv2.threshold(gray_img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
@@ -67,15 +82,40 @@ class ImageLineDrawer:
         min_y = min(self.points[0][1], self.points[1][1], self.points[2][1], self.points[3][1])
         max_y = max(self.points[0][1], self.points[1][1], self.points[2][1], self.points[3][1])
 
+        inter_x, inter_y = self.find_intersection()
+        intersection_point_relative = np.array([int((inter_x - min_x) / self.multiplier),
+                                                int((inter_y - min_y) / self.multiplier)])
+
         # Crop the image to the bounding box
         cropped_image = cv2.resize(img_thresh[min_y:max_y, min_x:max_x],
-                                   (0, 0), fx=1/self.multiplier, fy=1/self.multiplier)
-
+                                   (0, 0), fx=1 / self.multiplier, fy=1 / self.multiplier)
+        # Writing the template to the Template Folder
         cv2.imwrite(image_path, cropped_image)
+        np.savetxt('Data/Template/intersection.txt', intersection_point_relative)
         print(f"Region of the image overlapping with lines saved as {image_path}")
+
+    def find_intersection(self):
+        """
+        Given four points, determine the intersection point
+        :return: x, y coordinate of intersection point
+        """
+        x1, y1 = self.points[0]
+        x2, y2 = self.points[1]
+        x3, y3 = self.points[2]
+        x4, y4 = self.points[3]
+
+        # Calculate intersection point
+        intersection_x = ((x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4)) / \
+                         ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4))
+        intersection_y = ((x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4)) / \
+                         ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4))
+        return int(intersection_x), int(intersection_y)
 
 
 if __name__ == "__main__":
+    """
+    Argument parsing --> Driver code 
+    """
     parser = argparse.ArgumentParser(description='Draw lines on an image')
     parser.add_argument('image_path', type=str, help='Path to the input image file')
     parser.add_argument('save_path', type=str, help='Path to save image')
