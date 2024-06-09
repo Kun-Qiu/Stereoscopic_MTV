88d6929582c3b6d5c8fa56a5b938aae0a323f968
Kun Qiu <kun.qiu@vanderbilt.edu>
2024-06-09 11:26:24 -0500

    layout change

--- TemplateMatching.py
+++ TemplateMatching.py
@@ -3,30 +3,107 @@ import numpy as np
 from imutils.object_detection import non_max_suppression
 import argparse
 import Utility.Polygon as pl
+import Utility.Template as tm
+import matplotlib.pyplot as plt
+
+
+def match_template(image, template, intersection, polygon, dx=2 / 3):
+    """
+    Using OpenCV template matching module, the template is being matched to the
+    image to find location where the similarity is the strongest
+    :param  image: The image in which the template matching is applied toward
+    :param  template: The template in which the source image is compared against
+    :param  intersection: The spatial position of the intersection point
+    :param  polygon: The spatial constraint for filtering out false positive detections
+    :param  dx: Window where the non-max suppression is applied toward
+    :return: List of (x, y) coordinates where similarity is above threshold value
+    """
+    W, H = template.shape[:2]
+    rects = []
+
+    gray_source = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) \
+        if len(image.shape) == 3 else image
+    gray_template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY) \
+        if len(template.shape) == 3 else template
+
+    img_blur = cv2.GaussianBlur(gray_source, (5, 5), 0)
+    _, img_thresh = cv2.threshold(img_blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+    template_blur = cv2.GaussianBlur(gray_template, (5, 5), 0)
+    _, template_thresh = cv2.threshold(template_blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+    match = cv2.matchTemplate(image=img_thresh,
+                              templ=template_thresh,
+                              method=cv2.TM_CCOEFF_NORMED)
+
+    # Minimum correlation threshold
+    thresh = 0.4
+    temp_y, temp_x = np.where(match >= thresh)
+
+    for (x, y) in zip(temp_x, temp_y):
+        rects.append((x, y, x + dx * H, y + dx * W))
+
+    # Apply non-maxima suppression to the rectangles
+    pick = non_max_suppression(np.array(rects))
+    print("[INFO] {} matched locations *after* NMS".format(len(pick)))
+
+    inter_pos = []
+    for (startX, startY, endX, endY) in pick:
+        x_inter = int(startX + intersection[0])
+        y_inter = int(startY + intersection[1])
+        if polygon.contains(pl.Point(x_inter, y_inter)):
+            inter_pos.append((x_inter, y_inter))
+
+    return inter_pos
 
 
 class TemplateMatcher:
-    def __init__(self, source_image_path, template_path, intersection_path):
+    def __init__(self, source_image_path, target_image_path,
+                 template_path=None, intersection_path=None):
         """
         Class initialization for the template matcher
-        :param source_image_path: path to source image
-        :param template_path: path to template image
-        :param intersection_path: path to intersection txt
+        :param source_image_path: Path to source image
+        :param target_image_path: Path to target image
+        :param template_path: Path to template image (Default is None)
+        :param intersection_path: Path to intersection txt (Default is None)
+        :return A Template Matching Object
         """
 
         self._source = cv2.imread(source_image_path)
-        self._template = cv2.imread(template_path)
-        self._intersection = np.loadtxt(intersection_path)
-        self._matchedXCoordPreNMS = []
-        self._matchedYCoordPreNMS = []
-        self._matchedXCoordPostNMS = []
-        self._matchedYCoordPostNMS = []
+        self._target = cv2.imread(target_image_path)
+
+        self._template = None
+        self._intersection = None
+
+        if template_path is not None or intersection_path is not None:
+            self._template = cv2.imread(template_path)
+        else:
+            template_object = tm.Template(source_image_path, 2)
+            template = template_object.run()
+            self._template = template[0]
+            self._intersection = template[1]
+
+        """
+        Array contains the following information:
+        [[(x_s, y_s)], [(x_t, y_t)]] where s signifies position at source image
+                                           t signifies position at target image  
+        """
+        self._source_points = None
+        self._target_points = None
+
+        # self._matchedXCoordPreNMS = []
+        # self._matchedYCoordPreNMS = []
+        # self._matchedXCoordPostNMS = []
+        # self._matchedYCoordPostNMS = []
+
         self._polygon = None
 
     def set_boundary(self):
         """
-        Obtain the boundary and the edges of the polygon
-        :return: None
+        For easier detection of the relevant points, the user is asked to plot the vertices of the polygon
+        that encompasses the region of interest. The boundary and the edges of the polygon is obtained to
+        assist the filtering of the detected intersection.
+        :return: A boundary object
         """
         points = []
         img_clone = self._source.copy()
@@ -46,127 +123,149 @@ class TemplateMatcher:
         while True:
             key = cv2.waitKey(1) & 0xFF
             if key == ord("q"):  # press 'q' to quit picking points
-                self._polygon = pl.Polygon(points)
                 break
         cv2.destroyAllWindows()
 
-    def match_template(self):
-        """
-        Main driver for template matching using openCV
-        Since float is being converted into int for the intersections, the error accumulates
-        exponentially as the scale_num increase
-        :return: None
-        """
-        gray_source = cv2.cvtColor(self._source, cv2.COLOR_BGR2GRAY) \
-            if len(self._source.shape) == 3 else self._source
+        return pl.Polygon(points)
 
-        img_blur = cv2.GaussianBlur(gray_source, (5, 5), 0)
-        _, img_thresh = cv2.threshold(img_blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-        gray_template = cv2.cvtColor(self._template, cv2.COLOR_BGR2GRAY)
-
-        match = cv2.matchTemplate(image=img_thresh,
-                                  templ=gray_template,
-                                  method=cv2.TM_CCOEFF_NORMED)
-
-        # Minimum correlation threshold
-        thresh = 0.4
-        temp_y, temp_x = np.where(match >= thresh)
-        for i in range(len(temp_y)):
-            self._matchedYCoordPreNMS.append(temp_y[i])
-            self._matchedXCoordPreNMS.append(temp_x[i])
-
-        self.NonMaxSuppression()
-
-    def visualizeMatchBeforeNMS(self):
-        """
-        Visualize the match before NMS
-        :return: Visualize the match
-        """
-        clone = self._source.copy()
-        print("[INFO] {} matched locations *before* NMS".format(len(self._matchedYCoordPreNMS)))
-
-        for (x, y) in zip(self._matchedXCoordPreNMS, self._matchedYCoordPreNMS):
-            cv2.circle(clone, (int(x + self._intersection[0]),
-                               int(y + self._intersection[1])),
-                       2, (0, 255, 0), -1)
-
-        cv2.namedWindow("Before NMS", cv2.WINDOW_NORMAL)
-        cv2.resizeWindow("Before NMS", 800, 600)
-        cv2.imshow("Before NMS", clone)
-        cv2.waitKey(0)
-
-    def NonMaxSuppression(self, dx=2 / 3):
-        """
-        Reduce the amount of overlapping points in the template matching process
-        :param dx: size of the rectangle to check for overlap
-        :return: None
-        """
-
-        W, H = self._template.shape[:2]
-        rects = []
-
-        for (x, y) in zip(self._matchedXCoordPreNMS, self._matchedYCoordPreNMS):
-            rects.append((x, y, x + dx * H, y + dx * W))
-
-        # apply non-maxima suppression to the rectangles
-        pick = non_max_suppression(np.array(rects))
-        print("[INFO] {} matched locations *after* NMS".format(len(pick)))
-
-        for (startX, startY, endX, endY) in pick:
-            x_inter = int(startX + self._intersection[0])
-            y_inter = int(startY + self._intersection[1])
-            if self._polygon.contains(pl.Point(x_inter, y_inter)):
-                self._matchedXCoordPostNMS.append(x_inter)
-                self._matchedYCoordPostNMS.append(y_inter)
-
-    def visualizeMatchAfterNonMaxSuppression(self):
+    def visualize_match(self, source_points, target_points):
         """
         Visualize the match after NMS
-        :param dx: size of the rectangle to check for overlap
+        :param source_points: List of tuples [(x, y), (x1, y1), ...] representing source points
+        :param target_points: List of tuples [(x, y), (x1, y1), ...] representing target points
         :return: None
         """
 
-        clone = self._source.copy()
-        for i in range(len(self._matchedXCoordPostNMS)):
-            cv2.circle(clone, (self._matchedXCoordPostNMS[i], self._matchedYCoordPostNMS[i]),
-                       2, (0, 255, 0), -1)
-        cv2.namedWindow("After NMS", cv2.WINDOW_NORMAL)
-        cv2.resizeWindow("After NMS", 800, 600)
-        cv2.imshow("After NMS", clone)
-        cv2.waitKey(0)
+        # Convert the image from BGR to RGB (since OpenCV loads images in BGR)
+        image_rgb = cv2.cvtColor(self._source, cv2.COLOR_BGR2RGB)
 
-    def matching_displacement(self, target):
+        # Create a plot
+        plt.figure(figsize=(10, 8))
+        plt.imshow(image_rgb)
+
+        # Plot the source points
+        if source_points:
+            source_x, source_y = zip(*source_points)
+            plt.scatter(source_x, source_y, c='red', label='Source Points', s=40, edgecolor='black')
+
+        # Plot the target points
+        if target_points:
+            target_x, target_y = zip(*target_points)
+            plt.scatter(target_x, target_y, c='blue', label='Target Points', s=40, edgecolor='black')
+
+        # Add a legend
+        plt.legend()
+
+        # Show the plot
+        plt.title("Approximation of the intersection position")
+        plt.axis('off')  # Hide the axes
+        plt.show()
+
+    # def matching_displacement(self, source, target):
+    #     """
+    #     Find the correspondence between two set of intersection points
+    #     on the image
+    #     :param source: Intersections within the source image
+    #     :param target: Intersections within the target image
+    #     :return: A correspondence between the intersection points
+    #     """
+    #     distance_thresh = 0.25 * self.get_length()
+    #
+    #     # Construct arrays of coordinates for each point
+    #     source = np.column_stack((self._matchedXCoordPostNMS,
+    #                               self._matchedYCoordPostNMS))
+    #     target = np.column_stack((target.get_x_coord(),
+    #                               target.get_y_coord()))
+    #
+    #     correspondence = []
+    #     for x in source:
+    #         dist_x = []
+    #         for y in target:
+    #             dist_x.append(np.sqrt((y[0] - x[0]) ** 2 + (y[1] - x[1]) ** 2))
+    #
+    #         min_index = np.argmin(dist_x)  # Find the minimum distance for the current source point
+    #         if dist_x[min_index] < distance_thresh:
+    #             correspondence.append([x, target[min_index]])
+    #
+    #     return correspondence
+
+    def match_template_driver(self):
         """
-        Find the correspondence between two set of intersection points
-        on the image
-        :param target: target object
-        :return: A correspondence between the intersection points
+        Driver function for the template matching algorithm using the OpenCV Module
+        :return: None
         """
-        distance_thresh = 0.25 * self.get_length()
+        self._polygon = self.set_boundary()
+        self._source_points = match_template(self._source, self._template,
+                                             self._intersection, self._polygon)
+        self._target_points = match_template(self._target, self._template,
+                                             self._intersection, self._template)
 
-        # Construct arrays of coordinates for each point
-        source = np.column_stack((self._matchedXCoordPostNMS,
-                                  self._matchedYCoordPostNMS))
-        target = np.column_stack((target.get_x_coord(),
-                                  target.get_y_coord()))
+        self.visualize_match(self._source_points, self._target_points)
 
-        correspondence = []
-        for x in source:
-            dist_x = []
-            for y in target:
-                dist_x.append(np.sqrt((y[0] - x[0]) ** 2 + (y[1] - x[1]) ** 2))
+    # def visualizeMatchBeforeNMS(self):
+    #     """
+    #     Visualize the match before NMS
+    #     :return: Visualize the match
+    #     """
+    #     clone = self._source.copy()
+    #     print("[INFO] {} matched locations *before* NMS".format(len(self._matchedYCoordPreNMS)))
+    #
+    #     for (x, y) in zip(self._matchedXCoordPreNMS, self._matchedYCoordPreNMS):
+    #         cv2.circle(clone, (int(x + self._intersection[0]),
+    #                            int(y + self._intersection[1])),
+    #                    2, (0, 255, 0), -1)
+    #
+    #     cv2.namedWindow("Before NMS", cv2.WINDOW_NORMAL)
+    #     cv2.resizeWindow("Before NMS", 800, 600)
+    #     cv2.imshow("Before NMS", clone)
+    #     cv2.waitKey(0)
 
-            min_index = np.argmin(dist_x)  # Find the minimum distance for the current source point
-            if dist_x[min_index] < distance_thresh:
-                correspondence.append([x, target[min_index]])
+    # def NonMaxSuppression(self, dx=2 / 3):
+    #     """
+    #     Reduce the amount of overlapping points in the template matching process
+    #     :param dx: size of the rectangle to check for overlap
+    #     :return: None
+    #     """
+    #
+    #     W, H = self._template.shape[:2]
+    #     rects = []
+    #
+    #     for (x, y) in zip(self._matchedXCoordPreNMS, self._matchedYCoordPreNMS):
+    #         rects.append((x, y, x + dx * H, y + dx * W))
+    #
+    #     # apply non-maxima suppression to the rectangles
+    #     pick = non_max_suppression(np.array(rects))
+    #     print("[INFO] {} matched locations *after* NMS".format(len(pick)))
+    #
+    #     for (startX, startY, endX, endY) in pick:
+    #         x_inter = int(startX + self._intersection[0])
+    #         y_inter = int(startY + self._intersection[1])
+    #         if self._polygon.contains(pl.Point(x_inter, y_inter)):
+    #             self._matchedXCoordPostNMS.append(x_inter)
+    #             self._matchedYCoordPostNMS.append(y_inter)
 
-        return correspondence
+    # def visualizeMatchAfterNonMaxSuppression(self, source_points, target_points):
+    #     """
+    #     Visualize the match after NMS
+    #     :param source_points
+    #     :param target_points
+    #     :return: None
+    #     """
+    #
+    #     clone = self._source.copy()
+    #     for i in range(len(self._matchedXCoordPostNMS)):
+    #         cv2.circle(clone, (self._matchedXCoordPostNMS[i], self._matchedYCoordPostNMS[i]),
+    #                    2, (0, 255, 0), -1)
+    #     cv2.namedWindow("After NMS", cv2.WINDOW_NORMAL)
+    #     cv2.resizeWindow("After NMS", 800, 600)
+    #     cv2.imshow("After NMS", clone)
+    #     cv2.waitKey(0)
 
-    def get_x_coord(self):
-        return self._matchedXCoordPostNMS
-
-    def get_y_coord(self):
-        return self._matchedYCoordPostNMS
+    # def get_x_coord(self):
+    #     return self._matchedXCoordPostNMS
+    #
+    # def get_y_coord(self):
+    #     return self._matchedYCoordPostNMS
 
     def get_length(self):
         return self._intersection[2]
@@ -174,33 +273,35 @@ class TemplateMatcher:
     def get_boundary(self):
         return self._polygon
 
-    def run(self):
-        """
-        Run function for the template matching
-        :return: None
-        """
-        self.set_boundary()
-        self.match_template()
-        self.visualizeMatchBeforeNMS()
-        self.visualizeMatchAfterNonMaxSuppression()
-        cv2.destroyAllWindows()
+    # def run(self):
+    #     """
+    #     Run function for the template matching
+    #     :return: None
+    #     """
+    #     self.set_boundary()
+    #     self.match_template()
+    #     self.visualizeMatchBeforeNMS()
+    #     self.visualizeMatchAfterNonMaxSuppression()
+    #     cv2.destroyAllWindows()
 
 
 def main():
     parser = argparse.ArgumentParser(description="Template Matching")
     parser.add_argument("source_image", help="Path to the source image")
-    parser.add_argument("template", help="Path to the template image")
-    parser.add_argument("intersection", help="Path to the intersection file")
+    parser.add_argument("target_image", help="Path to the target image")
+    parser.add_argument("template", nargs='?', default=None, help="Path to the template image (optional)")
+    parser.add_argument("intersection", nargs='?', default=None, help="Path to the intersection file (optional)")
     args = parser.parse_args()
 
     """
     Sample visualization of the result of template matching on the image
     """
-    matcher = TemplateMatcher(args.source_image, args.template, args.intersection)
+    matcher = TemplateMatcher(args.source_image, args.target_image,
+                              args.template, args.intersection)
     matcher.set_boundary()
-    matcher.match_template()
-    matcher.visualizeMatchBeforeNMS()
-    matcher.visualizeMatchAfterNonMaxSuppression()
+    matcher.match_template_driver()
+    # matcher.visualizeMatchBeforeNMS()
+    # matcher.visualizeMatchAfterNonMaxSuppression()
     cv2.destroyAllWindows()
 
 
--- Utility/Template.py
+++ Utility/Template.py
@@ -8,7 +8,7 @@ template image for the template matching
 """
 
 """
-Utility Function
+Static Utility Function
 """
 
 
@@ -32,30 +32,33 @@ def correspondence_displacement(correspondence_list):
     return displacement_list
 
 
-class ImageLineDrawer:
-    def __init__(self, image_path, size_multiplier):
+def obtain_line(img, pt1, pt2, color=(0, 255, 0), thickness=4):
+    """
+    Using the OpenCV line function, a line is drawn on the input image
+    :param img: image
+    :param pt1: first point on the line
+    :param pt2: second point on the line
+    :param color: color of the line
+    :param thickness: thickness of the line
+    :return: draw the line on the input img
+    """
+    return cv2.line(img, pt1, pt2, color, thickness)
+
+
+class Template:
+    def __init__(self, image_path, size_multiplier=2):
         self.image = cv2.imread(image_path)
         self.resized_image = cv2.resize(self.image, (0, 0), fx=size_multiplier, fy=size_multiplier)
         self.points = []
         self.multiplier = size_multiplier
 
-    def draw_line(self, img, pt1, pt2, color=(0, 255, 0), thickness=4):
+    def user_prompted_points(self):
         """
-        :param img: image
-        :param pt1: first point on the line
-        :param pt2: second point on the line
-        :param color: color of the line
-        :param thickness: thickness of the line
-        :return: draw the line on the input img
-        """
-        return cv2.line(img, pt1, pt2, color, thickness)
-
-    def get_points(self):
-        """
-        Function allows the user to select points on the image
+        Allows the user to select the intersection points on the source image to
+        extract a template for the template matching algorithm
         :return: None
         """
-        print("Please pick two points on the image by clicking on them. Press 'q' to quit.")
+        print("Please pick four points on the image by clicking on them. Press 'q' to quit.")
 
         def mouse_callback(event, x, y, flags, param):
             if event == cv2.EVENT_LBUTTONDOWN:
@@ -69,37 +72,37 @@ class ImageLineDrawer:
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
-    def draw_lines(self):
+    def draw_lines_on_image(self):
         """
-        Lines are drawn on the image using the points obtained
-        in mouse click event
+        Ask the user to choose one intersection to be selected as the template, draw
+        the lines in the image
         :return: None
         """
-        self.get_points()
+        self.user_prompted_points()
         copied_image = self.resized_image.copy()
-        self.draw_line(copied_image,
-                       self.points[0],
-                       self.points[1])
 
-        self.draw_line(copied_image,
-                       self.points[2],
-                       self.points[3])
+        obtain_line(copied_image,
+                    self.points[0],
+                    self.points[1])
+
+        obtain_line(copied_image,
+                    self.points[2],
+                    self.points[3])
 
         cv2.imshow('Final Image', copied_image)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
-    def save_lines_image(self, image_path):
+    def save_lines_image(self):
+        # def save_lines_image(self, image_path):
         """
         Draw the lines on the image, crop the desired section for template,
         save the intersection point.
-        :param image_path: Path which image will be saved
-        :return: None
+        :return: [Template, Intersection Position]
         """
-        self.draw_lines()
 
-        gray_img = cv2.cvtColor(self.resized_image, cv2.COLOR_RGB2GRAY)
-        _, img_thresh = cv2.threshold(gray_img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        # gray_img = cv2.cvtColor(self.resized_image, cv2.COLOR_RGB2GRAY)
+        # _, img_thresh = cv2.threshold(gray_img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
         min_x = min(self.points[0][0], self.points[1][0], self.points[2][0], self.points[3][0])
         max_x = max(self.points[0][0], self.points[1][0], self.points[2][0], self.points[3][0])
@@ -116,12 +119,15 @@ class ImageLineDrawer:
                                                 int(length)])
 
         # Crop the image to the bounding box
-        cropped_image = cv2.resize(img_thresh[min_y:max_y, min_x:max_x],
+        cropped_image = cv2.resize(self.resized_image[min_y:max_y, min_x:max_x],
                                    (0, 0), fx=1 / self.multiplier, fy=1 / self.multiplier)
-        # Writing the template to the Template Folder
-        cv2.imwrite(image_path, cropped_image)
-        np.savetxt('Data/Template/intersection.txt', intersection_point_relative)
-        print(f"Region of the image overlapping with lines saved as {image_path}")
+
+        # # Writing the template to the Template Folder
+        # cv2.imwrite(image_path, cropped_image)
+        # np.savetxt('Data/Template/intersection.txt', intersection_point_relative)
+        # print(f"Region of the image overlapping with lines saved as {image_path}")
+
+        return [cropped_image, intersection_point_relative]
 
     def find_intersection(self):
         """
@@ -140,6 +146,15 @@ class ImageLineDrawer:
                          ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4))
         return int(intersection_x), int(intersection_y)
 
+    def run(self):
+        """
+        Run the necessary function to generate the template and the
+        intersection points
+        :return: None
+        """
+        self.draw_lines_on_image()
+        self.save_lines_image()
+
 
 if __name__ == "__main__":
     """
@@ -154,5 +169,5 @@ if __name__ == "__main__":
                         help='Multiplier to resize the image (default: 2)')
     args = parser.parse_args()
 
-    image_drawer = ImageLineDrawer(args.image_path, args.multi)
-    image_drawer.save_lines_image(args.save_path)
+    image_drawer = Template(args.image_path, args.multi)
+    image_drawer.run()
