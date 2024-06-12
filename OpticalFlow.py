e33976ce22ccef1a0573614a0c328cd4b5c81ff4
Kun Qiu <kun.qiu@vanderbilt.edu>
2024-06-12 14:48:10 -0500

    Attempt with Wavelets

--- OpticalFlow.py
+++ OpticalFlow.py
@@ -1,4 +1,4 @@
-import cv2 as cv
+import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 from Utility.DeNoise import denoised_image
@@ -16,22 +16,22 @@ class OpticalFlow:
         target_img = denoised_image(self._TARGET_PATH)
 
         # Calculate optical flow
-        self._flow = cv.calcOpticalFlowFarneback(source_img, target_img, None,
-                                                 pyr_scale=0.5,
-                                                 levels=3,
-                                                 winsize=15,
-                                                 iterations=10,
-                                                 poly_n=7,
-                                                 poly_sigma=1.5,
-                                                 flags=0)
+        self._flow = cv2.calcOpticalFlowFarneback(source_img, target_img, None,
+                                                  pyr_scale=0.5,
+                                                  levels=3,
+                                                  winsize=15,
+                                                  iterations=10,
+                                                  poly_n=10,
+                                                  poly_sigma=1.5,
+                                                  flags=0)
 
     def visualize_flow(self):
-        source_img = cv.imread(self._SOURCE_PATH)
-        magnitude, angle = cv.cartToPolar(self._flow[..., 0],
-                                          self._flow[..., 1])
+        source_img = cv2.imread(self._SOURCE_PATH)
+        magnitude, angle = cv2.cartToPolar(self._flow[..., 0],
+                                           self._flow[..., 1])
 
         # Plot optical flow vectors on the source image
-        plt.imshow(cv.cvtColor(source_img, cv.COLOR_BGR2RGB))
+        plt.imshow(cv2.cvtColor(source_img, cv2.COLOR_BGR2RGB))
         plt.quiver(range(0, self._flow.shape[0], 10),
                    range(0, self._flow.shape[1], 10),
                    self._flow[::10, ::10, 0],  # u component of flow
@@ -43,3 +43,10 @@ class OpticalFlow:
 
     def get_flow(self):
         return np.stack((self._flow[:, :, 0], self._flow[:, :, 1]), axis=-1)
+
+
+source_path = 'Data/Source/frame_0.png'
+target_path = 'Data/Target/frame_0_2us.png'
+optical_flow_object = OpticalFlow(source_path, target_path)
+optical_flow_object.calculate_optical_flow()
+optical_flow_object.visualize_flow()
