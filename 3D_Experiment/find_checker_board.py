3da6c2880a4697b3359400361ea7078d768fbb54
Kun Qiu <kun.qiu@vanderbilt.edu>
2024-07-07 15:02:23 -0500

    3D Coordinate Extrapolation

--- 3D_Experiment/find_checker_board.py
+++ 3D_Experiment/find_checker_board.py
@@ -1,123 +1,172 @@
 import numpy as np
-import matplotlib.pyplot as plt
+import re
 import cv2
-import non_linear_least_square as nnl
+import non_linear_least_square as nsl
+import os
+import matplotlib.pyplot as plt
+
+
+def get_sorted_corner(corners):
+    sorted_x_corners = sorted(corners, key=lambda p: p[0])
+    sorted_corners = []
+
+    index = 0
+    cur_x_val = sorted_x_corners[0][0]
+    for i in range(len(sorted_x_corners)):
+        x, _ = np.intp(sorted_x_corners[i])
+        if np.abs(x - cur_x_val) >= 5:
+            if x != cur_x_val:
+                sorted_corners.extend(sorted(sorted_x_corners[index:i], key=lambda p: p[1]))
+                index = i
+                cur_x_val = x
+
+    # Adding the last segment of corners
+    sorted_corners.extend(sorted(sorted_x_corners[index:], key=lambda p: p[1]))
+
+    return sorted_corners
+
+
+def manual_detection_corners(img, detected_corners, handle_mouse_bool=False):
+    img_copy = img.copy()
+    for corner in detected_corners:
+        x, y = np.intp(corner)
+        cv2.circle(img_copy, (x, y), 5, (0, 255, 0), -1)  # Green dots for refined corners
+
+    cv2.namedWindow('Corners', cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('Corners', 512, 512)
+    cv2.imshow('Corners', img_copy)
+
+    if handle_mouse_bool:
+        def handle_mouse(event, x, y, flags, param):
+            nonlocal img_copy, detected_corners
+
+            if event == cv2.EVENT_LBUTTONDOWN:
+                detected_corners = np.vstack([detected_corners, [x, y]])
+                cv2.circle(img_copy, (x, y), 5, (0, 0, 255), -1)
+                cv2.imshow('Corners', img_copy)
+
+            elif event == cv2.EVENT_RBUTTONDOWN:
+                # Check distance to each existing corner to determine if clicked
+                for i, corner in enumerate(detected_corners):
+                    dist = np.sqrt((x - corner[0]) ** 2 + (y - corner[1]) ** 2)
+                    if dist <= 5:  # 5 pixels threshold for clicking on a corner
+                        detected_corners = np.delete(detected_corners, i, axis=0)
+                        img_copy = img.copy()  # Reset image copy
+                        for corner in detected_corners:
+                            cv2.circle(img_copy, np.intp(corner), 5, (0, 255, 0), -1)
+                        cv2.imshow('Corners', img_copy)
+
+        # Create a resizable named window
+        cv2.setMouseCallback('Corners', handle_mouse)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    return detected_corners
 
 
 class CornerDetector:
-    def __init__(self, distorted_img):
-        self.img = cv2.imread(distorted_img)
-        # self.calibrate_img = cv2.imread(calibration_img)
-        self.corners = None
-        self._optimized_param = None
+    def __init__(self, image_set_path, calibrate_shape=(10, 10)):
+        assert len(image_set_path) >= 1, "No path is given for the calibration images."
 
-    def detect_corners(self):
-        gray = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
-        gray = np.float32(gray)
-        dst = cv2.cornerHarris(gray, 2, 3, 0.04)
-        dst = cv2.dilate(dst, None)
-        ret, dst = cv2.threshold(dst, 0.01 * dst.max(), 255, 0)
-        dst = np.uint8(dst)
+        self.__left_image_set = []
+        self.__right_image_set = []
+        for filename in image_set_path:
+            match = re.search(r'([-]?\d+)mm', filename)
+            if match:
+                if 'left' in filename.lower():
+                    self.__left_image_set.append([filename, int(match.group(1))])
+                elif 'right' in filename.lower():
+                    self.__right_image_set.append([filename, int(match.group(1))])
 
-        # find centroids
-        ret, labels, stats, centroids = cv2.connectedComponentsWithStats(dst)
+        self.__left_image_set = np.array(self.__left_image_set, dtype=object)
+        self.__right_image_set = np.array(self.__right_image_set, dtype=object)
+        self.__calibrate_shape = calibrate_shape
+        self.__optimized_param = None
 
-        # define the criteria to stop and refine the corners
-        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
-        corners = cv2.cornerSubPix(gray, np.float32(centroids), (5, 5), (-1, -1), criteria)
+    def __detect_corners(self, image_set_path):
+        assert isinstance(image_set_path, np.ndarray), "Incorrect array structure: requires Numpy array."
 
-        self.corners = corners
+        all_img_corners = []
+        for image in image_set_path[:, 0]:
+            img = cv2.imread(image)
 
-    def visualize_corners(self, handle_mouse_bool=False):
-        img_copy = self.img.copy()
-        # Draw corners on the image
-        for corner in self.corners:
-            x, y = np.intp(corner)
-            cv2.circle(img_copy, (x, y), 5, (0, 255, 0), -1)  # Green dots for refined corners
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            gray = np.float32(gray)
+            dst = cv2.cornerHarris(gray, 2, 3, 0.04)
+            dst = cv2.dilate(dst, None)
+            ret, dst = cv2.threshold(dst, 0.01 * dst.max(), 255, 0)
+            dst = np.uint8(dst)
 
-        cv2.namedWindow('Corners', cv2.WINDOW_NORMAL)
-        cv2.resizeWindow('Corners', 512, 512)
-        cv2.imshow('Corners', img_copy)
+            # find centroids
+            ret, labels, stats, centroids = cv2.connectedComponentsWithStats(dst)
 
-        if handle_mouse_bool:
-            def handle_mouse(event, x, y, flags, param):
-                nonlocal img_copy
+            # define the criteria to stop and refine the corners
+            criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
+            corners = cv2.cornerSubPix(gray, np.float32(centroids), (11, 11), (-1, -1), criteria)
+            single_img_corners = manual_detection_corners(img, corners, handle_mouse_bool=True)
+            sorted_corners = np.array(get_sorted_corner(single_img_corners))
 
-                if event == cv2.EVENT_LBUTTONDOWN:
-                    self.corners = np.vstack([self.corners, [x, y]])
-                    cv2.circle(img_copy, (x, y), 5, (0, 0, 255), -1)
-                    cv2.imshow('Corners', img_copy)
+            i = 0
+            while i < len(sorted_corners) - 1:
+                if np.sqrt((sorted_corners[i, 0] - sorted_corners[i + 1, 0]) ** 2 +
+                           (sorted_corners[i, 1] - sorted_corners[i + 1, 1]) ** 2) < 1:
+                    sorted_corners = np.delete(sorted_corners, i + 1, axis=0)
+                else:
+                    i += 1
+            all_img_corners.extend(sorted_corners)
 
-                elif event == cv2.EVENT_RBUTTONDOWN:
-                    # Check distance to each existing corner to determine if clicked
-                    for i, corner in enumerate(self.corners):
-                        dist = np.sqrt((x - corner[0]) ** 2 + (y - corner[1]) ** 2)
-                        if dist <= 5:  # 5 pixels threshold for clicking on a corner
-                            self.corners = np.delete(self.corners, i, axis=0)
-                            img_copy = self.img.copy()  # Reset image copy
-                            for corner in self.corners:
-                                cv2.circle(img_copy, np.intp(corner), 5, (0, 255, 0), -1)
-                            cv2.imshow('Corners', img_copy)
+        all_img_corners = np.array(all_img_corners, dtype=object)
+        return all_img_corners, image_set_path[:, 1]
 
-            # Create a resizable named window
-            cv2.setMouseCallback('Corners', handle_mouse)
+    def __object_plane_coordinates(self, num_grid, z_coords):
+        step_x, step_y = self.__calibrate_shape
 
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        calibrate_coordinates = []
+        for z_coord in z_coords:
+            coordinates = [
+                [x, y, z_coord]
+                for x in np.arange(0, num_grid * step_x, step_x)
+                for y in np.arange(0, num_grid * step_y, step_y)
+            ]
+            calibrate_coordinates.extend(coordinates)
 
-    def get_sorted_corner(self):
-        sorted_x_corners = sorted(self.corners, key=lambda p: p[0])
-        sorted_corners = []
+        return calibrate_coordinates
 
-        index = 0
-        cur_x_val = sorted_x_corners[0][0]
-        for i in range(len(sorted_x_corners)):
-            x, _ = np.intp(sorted_x_corners[i])
-            if np.abs(x - cur_x_val) >= 5:
-                if x != cur_x_val:
-                    sorted_corners.extend(sorted(sorted_x_corners[index:i], key=lambda p: p[1]))
-                    index = i
-                    cur_x_val = x
+    def run(self):
+        calibrated_point = self.__object_plane_coordinates(11, self.__left_image_set[:, 1])
+        flattened_right, _ = self.__detect_corners(self.__right_image_set)
+        right_nsl_object = nsl.NonLinearLeastSquare(calibrated_points=calibrated_point,
+                                                    distorted_points=flattened_right)
 
-        # Adding the last segment of corners
-        sorted_corners.extend(sorted(sorted_x_corners[index:], key=lambda p: p[1]))
+        right_param = right_nsl_object.calculate_least_square()
 
-        return sorted_corners
+        # fig, ax = plt.subplots()
+        #
+        # # Plot flattened_right
+        # ax.scatter(flattened_right[:121, 0], flattened_right[:121, 1], color='blue', label='Flattened Right')
+        #
+        # # Plot predicted points
+        # for pt in calibrated_point[:121]:
+        #     xpredicted, ypredicted = nsl.project_object_image(19, right_param, pt)
+        #     ax.scatter(xpredicted, ypredicted, color='red', label='Predicted')
+        #
+        # ax.set_xlabel('X Coordinate')
+        # ax.set_ylabel('Y Coordinate')
+        # ax.legend()
+        # plt.title('Comparison of Predicted and Flattened Right Points')
+        # plt.show()
+
+        flattened_left, _ = self.__detect_corners(self.__left_image_set)
 
 
 # Example usage:
 if __name__ == "__main__":
-    detector = CornerDetector('../3D_Experiment/Calibration/right_35deg.png')
-    detector.detect_corners()
-    detector.visualize_corners(handle_mouse_bool=True)
-    distorted_corners = detector.get_sorted_corner()
+    directory = '../3D_Experiment/Calibration/'
+    image_filenames = []
+    for root, _, files in os.walk(directory):
+        for file in files:
+            if file.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.gif', '.tiff')):
+                image_filenames.append(os.path.join(root, file))
 
-    source_detector = CornerDetector('../3D_Experiment/Calibration/right_0deg.png')
-    source_detector.detect_corners()
-    source_detector.visualize_corners(handle_mouse_bool=True)
-    calibrate_corners = source_detector.get_sorted_corner()
-
-    distorted_corners = np.array(distorted_corners, dtype=np.intp)
-    calibrated_corners = np.array(calibrate_corners, dtype=np.intp)
-
-    # Plotting the correspondences
-    plt.figure(figsize=(10, 5))
-    plt.title('Correspondences between Distorted and Calibrate Corners')
-
-    # Plot distorted corners
-    plt.scatter(distorted_corners[:, 0], distorted_corners[:, 1], c='r', label='Distorted Corners')
-    # Plot calibrate corners
-    plt.scatter(calibrate_corners[:, 0], calibrate_corners[:, 1], c='b', label='Calibrate Corners')
-
-    # Draw lines between corresponding points
-    for i in range(len(distorted_corners)):
-        plt.plot([distorted_corners[i, 0], calibrate_corners[i, 0]],
-                 [distorted_corners[i, 1], calibrate_corners[i, 1]], 'g-')
-
-    plt.xlabel('X')
-    plt.ylabel('Y')
-    plt.legend()
-    plt.show()
-
-    param_object = nnl.NonLinearLeastSquare(calibrated_corners, distorted_corners)
-    param_op = param_object.calculate_least_square()
+    detector = CornerDetector(image_filenames, (40, 40))
+    detector.run()
--- 3D_Experiment/non_linear_least_square.py
+++ 3D_Experiment/non_linear_least_square.py
@@ -2,43 +2,206 @@ import numpy as np
 from scipy.optimize import least_squares
 
 
+def projection_object_to_image(num_params, params, point):
+    params = params.reshape(num_params, 2)
+    assert len(params) == 19, "Parameters must contain 19 coefficient vectors."
+
+    # Transformation Coefficient through Calibration
+    coeff_x = params[:, 0]
+    coeff_y = params[:, 1]
+    assert len(coeff_x) and len(coeff_y) == 19, "Coefficient must contain 19 values."
+
+    xi, yi, zi = np.intp(point)
+
+    # Define the polynomial model
+    x_predicted = (coeff_x[0] +
+                   (coeff_x[1] * xi) +
+                   (coeff_x[2] * yi) +
+                   (coeff_x[3] * zi) +
+                   (coeff_x[4] * (xi ** 2)) +
+                   (coeff_x[5] * xi * yi) +
+                   (coeff_x[6] * (yi ** 2)) +
+                   (coeff_x[7] * (xi * zi)) +
+                   (coeff_x[8] * (yi * zi)) +
+                   (coeff_x[9] * (zi ** 2)) +
+                   (coeff_x[10] * (xi ** 3)) +
+                   (coeff_x[11] * (xi ** 2) * yi) +
+                   (coeff_x[12] * xi * (yi ** 2)) +
+                   (coeff_x[13] * (yi ** 3)) +
+                   (coeff_x[14] * (xi ** 2) * zi) +
+                   (coeff_x[15] * xi * yi * zi) +
+                   (coeff_x[16] * (yi ** 2) * zi) +
+                   (coeff_x[17] * xi * (zi ** 2)) +
+                   (coeff_x[18] * yi * (zi ** 2)))
+
+    y_predicted = (coeff_y[0] +
+                   (coeff_y[1] * xi) +
+                   (coeff_y[2] * yi) +
+                   (coeff_y[3] * zi) +
+                   (coeff_y[4] * (xi ** 2)) +
+                   (coeff_y[5] * xi * yi) +
+                   (coeff_y[6] * (yi ** 2)) +
+                   (coeff_y[7] * (xi * zi)) +
+                   (coeff_y[8] * (yi * zi)) +
+                   (coeff_y[9] * (zi ** 2)) +
+                   (coeff_y[10] * (xi ** 3)) +
+                   (coeff_y[11] * (xi ** 2) * yi) +
+                   (coeff_y[12] * xi * (yi ** 2)) +
+                   (coeff_y[13] * (yi ** 3)) +
+                   (coeff_y[14] * (xi ** 2) * zi) +
+                   (coeff_y[15] * xi * yi * zi) +
+                   (coeff_y[16] * (yi ** 2) * zi) +
+                   (coeff_y[17] * xi * (zi ** 2)) +
+                   (coeff_y[18] * yi * (zi ** 2)))
+
+    return x_predicted, y_predicted
+
+
 class NonLinearLeastSquare:
-    def __init__(self, calibrated_points, distorted_points):
-        self._calibrated_points = calibrated_points
-        self._distorted_points = distorted_points
+    def __init__(self, calibrated_points=None, distorted_points=None):
+        assert len(calibrated_points) == len(distorted_points), "Length of calibration and distortion are not equal."
+        self.__calibrated_points = calibrated_points
+        self.__distorted_points = distorted_points
+        self.__NUM_PARAM = 19
+        self.__left_calibrate_param = np.zeros(self.__NUM_PARAM, 2)
+        self.__right_calibrate_param = np.zeros(self.__NUM_PARAM, 2)
 
-    def _projection_polynomial_model(self, point, params):
-        a11, a12, a13, a14, a15, a16, a21, a22, a23, a24, a25, a26, a31, a32, a34, a35, a36 = params
-        a33 = 1
-
-        xi, yi = np.intp(point)
-
-        denominator = (a31 * xi) + (a32 * yi) + a33 + (a34 * xi ** 2) + \
-                      (a35 * yi ** 2) + (a36 * xi * yi)
-
-        x_predicted = ((a11 * xi) + (a12 * yi) + a13 + (a14 * xi ** 2) + (a15 * yi ** 2) +
-                       (a16 * xi * yi)) / denominator
-
-        y_predicted = ((a21 * xi) + (a22 * yi) + a23 + (a24 * xi ** 2) + (a25 * yi ** 2) +
-                       (a26 * xi * yi)) / denominator
-        return x_predicted, y_predicted
-
-    def _residuals(self, params):
+    def __calibrate_residuals(self, params):
         x_predicted, y_predicted = [], []
-        for point in self._distorted_points:
-            x, y = self._projection_polynomial_model(point, params)
-            x_predicted.append(x)
-            y_predicted.append(y)
+        for point in self.__calibrated_points:
+            x_img, y_img = self.__calibration_polynomial(point, params)
+            x_predicted.append(x_img)
+            y_predicted.append(y_img)
+
         x_predicted = np.array(x_predicted)
         y_predicted = np.array(y_predicted)
 
-        x_true, y_true = zip(*self._calibrated_points)
+        x_true, y_true = zip(*self.__distorted_points)
         x_true = np.array(x_true)
         y_true = np.array(y_true)
 
-        return np.concatenate([x_predicted - x_true, y_predicted - y_true])
+        return np.concatenate([x_predicted - x_true, y_predicted - y_true]).flatten()
 
-    def calculate_least_square(self):
-        params = np.random.rand(17)
-        result = least_squares(self._residuals, params, method='lm')
+    def __calibration_polynomial(self, point, params):
+        params = params.reshape(self.__NUM_PARAM, 2)
+        assert len(params) == 19, "Parameters must contain 19 coefficient vectors."
+
+        # Transformation Coefficient through Calibration
+        coeff_x = params[:, 0]
+        coeff_y = params[:, 1]
+        assert len(coeff_x) and len(coeff_y) == 19, "Coefficient must contain 19 values."
+
+        xi, yi, zi = np.intp(point)
+
+        # Polynomial model
+        x_predicted = (coeff_x[0] +
+                       (coeff_x[1] * xi) +
+                       (coeff_x[2] * yi) +
+                       (coeff_x[3] * zi) +
+                       (coeff_x[4] * (xi ** 2)) +
+                       (coeff_x[5] * xi * yi) +
+                       (coeff_x[6] * (yi ** 2)) +
+                       (coeff_x[7] * (xi * zi)) +
+                       (coeff_x[8] * (yi * zi)) +
+                       (coeff_x[9] * (zi ** 2)) +
+                       (coeff_x[10] * (xi ** 3)) +
+                       (coeff_x[11] * (xi ** 2) * yi) +
+                       (coeff_x[12] * xi * (yi ** 2)) +
+                       (coeff_x[13] * (yi ** 3)) +
+                       (coeff_x[14] * (xi ** 2) * zi) +
+                       (coeff_x[15] * xi * yi * zi) +
+                       (coeff_x[16] * (yi ** 2) * zi) +
+                       (coeff_x[17] * xi * (zi ** 2)) +
+                       (coeff_x[18] * yi * (zi ** 2)))
+
+        y_predicted = (coeff_y[0] +
+                       (coeff_y[1] * xi) +
+                       (coeff_y[2] * yi) +
+                       (coeff_y[3] * zi) +
+                       (coeff_y[4] * (xi ** 2)) +
+                       (coeff_y[5] * xi * yi) +
+                       (coeff_y[6] * (yi ** 2)) +
+                       (coeff_y[7] * (xi * zi)) +
+                       (coeff_y[8] * (yi * zi)) +
+                       (coeff_y[9] * (zi ** 2)) +
+                       (coeff_y[10] * (xi ** 3)) +
+                       (coeff_y[11] * (xi ** 2) * yi) +
+                       (coeff_y[12] * xi * (yi ** 2)) +
+                       (coeff_y[13] * (yi ** 3)) +
+                       (coeff_y[14] * (xi ** 2) * zi) +
+                       (coeff_y[15] * xi * yi * zi) +
+                       (coeff_y[16] * (yi ** 2) * zi) +
+                       (coeff_y[17] * xi * (zi ** 2)) +
+                       (coeff_y[18] * yi * (zi ** 2)))
+
+        return x_predicted, y_predicted
+
+    def __inverse_residuals(self, object_pt, img_pt_left, img_pt_right, coeffs_left, coeffs_right):
+        assert img_pt_left.shape == img_pt_right.shape == (2,), "Shape of input image coordinates must be (2,)."
+
+        assert coeffs_left.shape == coeffs_right.shape == (self.__NUM_PARAM, 2), \
+            "Coefficients must be of shape (19, 2)."
+
+        xi, yi, zi = np.intp(object_pt)
+
+        coeff_x_left, coeff_y_left = coeffs_left[:, 0], coeffs_left[:, 1]
+        coeff_x_right, coeff_y_right = coeffs_right[:, 0], coeffs_right[:, 1]
+
+        x_predicted_left = (coeff_x_left[0] + coeff_x_left[1] * xi + coeff_x_left[2] * yi +
+                            coeff_x_left[3] * zi + coeff_x_left[4] * (xi ** 2) + coeff_x_left[5] * xi * yi +
+                            coeff_x_left[6] * (yi ** 2) + coeff_x_left[7] * (xi * zi) + coeff_x_left[8] * (yi * zi) +
+                            coeff_x_left[9] * (zi ** 2) + coeff_x_left[10] * (xi ** 3) +
+                            coeff_x_left[11] * (xi ** 2) * yi + coeff_x_left[12] * xi * (yi ** 2) +
+                            coeff_x_left[13] * (yi ** 3) + coeff_x_left[14] * (xi ** 2) * zi +
+                            coeff_x_left[15] * xi * yi * zi + coeff_x_left[16] * (yi ** 2) * zi +
+                            coeff_x_left[17] * xi * (zi ** 2) + coeff_x_left[18] * yi * (zi ** 2))
+
+        y_predicted_left = (coeff_y_left[0] + coeff_y_left[1] * xi + coeff_y_left[2] * yi +
+                            coeff_y_left[3] * zi + coeff_y_left[4] * (xi ** 2) + coeff_y_left[5] * xi * yi +
+                            coeff_y_left[6] * (yi ** 2) + coeff_y_left[7] * (xi * zi) + coeff_y_left[8] * (yi * zi) +
+                            coeff_y_left[9] * (zi ** 2) + coeff_y_left[10] * (xi ** 3) +
+                            coeff_y_left[11] * (xi ** 2) * yi + coeff_y_left[12] * xi * (yi ** 2) +
+                            coeff_y_left[13] * (yi ** 3) + coeff_y_left[14] * (xi ** 2) * zi +
+                            coeff_y_left[15] * xi * yi * zi + coeff_y_left[16] * (yi ** 2) * zi +
+                            coeff_y_left[17] * xi * (zi ** 2) + coeff_y_left[18] * yi * (zi ** 2))
+
+        x_predicted_right = (coeff_x_right[0] + coeff_x_right[1] * xi + coeff_x_right[2] * yi +
+                             coeff_x_right[3] * zi + coeff_x_right[4] * (xi ** 2) + coeff_x_right[5] * xi * yi +
+                             coeff_x_right[6] * (yi ** 2) + coeff_x_right[7] * (xi * zi) + coeff_x_right[8] * (
+                                     yi * zi) +
+                             coeff_x_right[9] * (zi ** 2) + coeff_x_right[10] * (xi ** 3) +
+                             coeff_x_right[11] * (xi ** 2) * yi + coeff_x_right[12] * xi * (yi ** 2) +
+                             coeff_x_right[13] * (yi ** 3) + coeff_x_right[14] * (xi ** 2) * zi +
+                             coeff_x_right[15] * xi * yi * zi + coeff_x_right[16] * (yi ** 2) * zi +
+                             coeff_x_right[17] * xi * (zi ** 2) + coeff_x_right[18] * yi * (zi ** 2))
+
+        y_predicted_right = (coeff_y_right[0] + coeff_y_right[1] * xi + coeff_y_right[2] * yi +
+                             coeff_y_right[3] * zi + coeff_y_right[4] * (xi ** 2) + coeff_y_right[5] * xi * yi +
+                             coeff_y_right[6] * (yi ** 2) + coeff_y_right[7] * (xi * zi) + coeff_y_right[8] * (
+                                     yi * zi) +
+                             coeff_y_right[9] * (zi ** 2) + coeff_y_right[10] * (xi ** 3) +
+                             coeff_y_right[11] * (xi ** 2) * yi + coeff_y_right[12] * xi * (yi ** 2) +
+                             coeff_y_right[13] * (yi ** 3) + coeff_y_right[14] * (xi ** 2) * zi +
+                             coeff_y_right[15] * xi * yi * zi + coeff_y_right[16] * (yi ** 2) * zi +
+                             coeff_y_right[17] * xi * (zi ** 2) + coeff_y_right[18] * yi * (zi ** 2))
+
+        residuals = [x_predicted_left - img_pt_left[0], y_predicted_left - img_pt_left[1],
+                     x_predicted_right - img_pt_right[0], y_predicted_right - img_pt_right[1]]
+        return residuals
+
+    def inverse_least_square(self, left_img_pts, right_img_pts, coeffs_left, coeffs_right):
+        object_pt_predicated = np.zeros(3)
+        result = least_squares(self.__inverse_residuals, object_pt_predicated, method='trf',
+                               args=(left_img_pts, right_img_pts, coeffs_left, coeffs_right))
         return result.x
+
+    def calibrate_least_square(self):
+        if self.__calibrated_points is None or self.__distorted_points is None:
+            raise ValueError(f"This function cannot be called if no input corners are given")
+        else:
+            params = np.zeros((self.__NUM_PARAM, 2))
+            result = least_squares(self.__calibrate_residuals, params.flatten(), method='trf')
+            return result.x.reshape((self.__NUM_PARAM, 2))
+
+    def get_right_calibration(self, right_object):
+        right_object.get_right_calibration
